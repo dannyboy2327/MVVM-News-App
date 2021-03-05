49f54775afbdc5da390a21db68bb486701a4c9d1
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-04 19:25:58 -0800

    Feat: Added the implementation for the host
    This activity hosts the fragments with fragmentManager and also takes care of handling bottom navigation clicks.

--- app/src/main/java/com/anomalydev/worldnewsforyou/MainActivity.kt
+++ app/src/main/java/com/anomalydev/worldnewsforyou/MainActivity.kt
@@ -2,12 +2,117 @@ package com.anomalydev.worldnewsforyou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
+import com.anomalydev.worldnewsforyou.databinding.ActivityMainBinding
+import com.anomalydev.worldnewsforyou.features.bookmarks.BookmarksFragment
+import com.anomalydev.worldnewsforyou.features.breakingnews.BreakingNewsFragment
+import com.anomalydev.worldnewsforyou.features.searchnews.SearchNewsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var breakingNewsFragment: BreakingNewsFragment
+    private lateinit var searchNewsFragment: SearchNewsFragment
+    private lateinit var bookmarksFragment: BookmarksFragment
+
+    private val fragments: Array<Fragment>
+        get() = arrayOf(
+            breakingNewsFragment,
+            searchNewsFragment,
+            bookmarksFragment
+        )
+
+    // This value will be used to navigate to selected fragment on config change
+    private var selectedIndex = 0
+
+    // This will be used to get the fragment at its selected position on config change
+    private val selectedFragment get() = fragments[selectedIndex]
+
+    /**
+     * This method takes care of selecting the fragment with its correct index to transition.
+     * Else it will detach from the transition.
+     */
+    private fun selectFragment(selectedFragment: Fragment) {
+        var transaction = supportFragmentManager.beginTransaction()
+        fragments.forEachIndexed { index, fragment ->
+            if (selectedFragment == fragment) {
+                transaction = transaction.attach(fragment)
+                selectedIndex = index
+            } else {
+               transaction = transaction.detach(fragment)
+            }
+        }
+        transaction.commit()
+
+        // Will set the title based on selected fragment
+        title = when (selectedFragment) {
+            is BreakingNewsFragment -> getString(R.string.title_breaking_news)
+            is SearchNewsFragment -> getString(R.string.title_search_news)
+            is BookmarksFragment -> getString(R.string.title_bookmarks)
+            else -> ""
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        if (savedInstanceState == null) {
+
+            breakingNewsFragment = BreakingNewsFragment()
+            searchNewsFragment = SearchNewsFragment()
+            bookmarksFragment = BookmarksFragment()
+
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, breakingNewsFragment, TAG_BREAKING_NEWS_FRAGMENT)
+                .add(R.id.fragment_container, searchNewsFragment, TAG_SEARCH_NEWS_FRAGMENT)
+                .add(R.id.fragment_container, bookmarksFragment, TAG_BOOKMARKS_FRAGMENT)
+                .commit()
+        } else {
+            breakingNewsFragment = supportFragmentManager.findFragmentByTag(TAG_BREAKING_NEWS_FRAGMENT) as BreakingNewsFragment
+            searchNewsFragment = supportFragmentManager.findFragmentByTag(TAG_SEARCH_NEWS_FRAGMENT) as SearchNewsFragment
+            bookmarksFragment = supportFragmentManager.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) as BookmarksFragment
+            selectedIndex = savedInstanceState.getInt(KEY_SELECTED_INDEX, 0)
+        }
+
+        selectFragment(selectedFragment)
+
+        // Will navigate to selected fragment on click on bottom nav
+        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.nav_breaking -> breakingNewsFragment
+                R.id.nav_search -> searchNewsFragment
+                R.id.nav_bookmarks -> bookmarksFragment
+                else -> throw IllegalArgumentException("Unexpected itemId")
+            }
+            selectFragment(fragment)
+            true
+        }
+    }
+
+    /**
+     *  Used to navigate to breaking news fragment on back press
+     */
+    override fun onBackPressed() {
+        if (selectedIndex != 0) {
+            binding.bottomNav.selectedItemId = R.id.nav_breaking
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    /**
+     * Used to save the instance of the selected index
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(KEY_SELECTED_INDEX, selectedIndex)
     }
 }
+
+private const val TAG_BREAKING_NEWS_FRAGMENT = "TAG_BREAKING_NEWS_FRAGMENT"
+private const val TAG_SEARCH_NEWS_FRAGMENT = "TAG_SEARCH_NEWS_FRAGMENT"
+private const val TAG_BOOKMARKS_FRAGMENT = "TAG_BOOKMARKS_FRAGMENT"
+private const val KEY_SELECTED_INDEX = "KEY_SELECTED_INDEX"
