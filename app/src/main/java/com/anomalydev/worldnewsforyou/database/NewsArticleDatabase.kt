9e27f9aee0508dc4cdc426c554484dcfcd2a9db4
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-05 13:43:38 -0800

    Feat: Added a database file
    This abstract class will be used to create the room database for the application.

---
+++ app/src/main/java/com/anomalydev/worldnewsforyou/database/NewsArticleDatabase.kt
@@ -0,0 +1,10 @@
+package com.anomalydev.worldnewsforyou.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [NewsArticle::class, BreakingNews::class], version = 1, exportSchema = false)
+abstract class NewsArticleDatabase: RoomDatabase() {
+
+    abstract fun newsArticleDao(): NewsArticleDao
+}
