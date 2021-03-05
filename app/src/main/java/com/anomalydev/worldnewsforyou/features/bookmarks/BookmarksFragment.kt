fd5b63a71f25589e0330fd37f97849f2cc15916b
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-04 19:18:13 -0800

    Feat: Added the fragment for navigation

---
+++ app/src/main/java/com/anomalydev/worldnewsforyou/features/bookmarks/BookmarksFragment.kt
@@ -0,0 +1,9 @@
+package com.anomalydev.worldnewsforyou.features.bookmarks
+
+import androidx.fragment.app.Fragment
+import com.anomalydev.worldnewsforyou.R
+
+class BookmarksFragment: Fragment(R.layout.fragment_bookmarks) {
+
+
+}
---
+++ app/src/main/java/com/anomalydev/worldnewsforyou/features/breakingnews/BreakingNewsFragment.kt
@@ -0,0 +1,9 @@
+package com.anomalydev.worldnewsforyou.features.breakingnews
+
+import androidx.fragment.app.Fragment
+import com.anomalydev.worldnewsforyou.R
+
+class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
+
+
+}
