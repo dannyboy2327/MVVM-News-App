656b5ee209f03fca8928b4320802c2a8c5438974
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-06 15:36:33 -0800

    Feat: Added a flow mutable and immutable val
    With backing property, this will allow the immutable to hold the list of news articles for the adapter.

--- app/src/main/java/com/anomalydev/worldnewsforyou/features/breakingnews/BreakingNewsViewModel.kt
+++ app/src/main/java/com/anomalydev/worldnewsforyou/features/breakingnews/BreakingNewsViewModel.kt
@@ -1,8 +1,13 @@
 package com.anomalydev.worldnewsforyou.features.breakingnews
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anomalydev.worldnewsforyou.database.NewsArticle
 import com.anomalydev.worldnewsforyou.database.NewsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +15,14 @@ class BreakingNewsViewModel @Inject constructor(
     private val repository: NewsRepository
 ): ViewModel() {
 
+    private val _breakingNewsFlow = MutableStateFlow<List<NewsArticle>>(emptyList())
+    val breakingNews: Flow<List<NewsArticle>>
+        get() = _breakingNewsFlow
 
+    init {
+        viewModelScope.launch {
+            val news = repository.getBreakingNews()
+            _breakingNewsFlow.value = news
+        }
+    }
 }
