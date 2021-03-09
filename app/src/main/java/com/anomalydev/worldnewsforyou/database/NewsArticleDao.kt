77c8bab4b9c300f9947e242a5beab51e0d161333
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-08 16:31:38 -0800

    Feat: Added a query to delete old articles
    The dao will get articles that are the oldest and delete them.

--- app/src/main/java/com/anomalydev/worldnewsforyou/database/NewsArticleDao.kt
+++ app/src/main/java/com/anomalydev/worldnewsforyou/database/NewsArticleDao.kt
@@ -17,4 +17,7 @@ interface NewsArticleDao {
 
     @Query("DELETE FROM breaking_news")
     suspend fun deleteAllBreakingNews()
+
+    @Query("DELETE FROM news_articles WHERE updatedAt < :timestampInMillis AND isBookmarked = 0")
+    suspend fun deleteNonBookmarkedArticlesOlderThan(timestampInMillis: Long)
 }
