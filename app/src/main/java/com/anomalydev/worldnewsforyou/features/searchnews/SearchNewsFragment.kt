0e0e9335bdd1d0201a088b1ae182562317da4086
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-10 15:00:49 -0800

    Feat: Added the loading state function
    This function will load the corresponding views depending on the loading state of the view. Also inflated the menu layout.

--- app/src/main/java/com/anomalydev/worldnewsforyou/features/searchnews/SearchNewsFragment.kt
+++ app/src/main/java/com/anomalydev/worldnewsforyou/features/searchnews/SearchNewsFragment.kt
@@ -3,16 +3,17 @@ package com.anomalydev.worldnewsforyou.features.searchnews
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anomalydev.worldnewsforyou.R
 import com.anomalydev.worldnewsforyou.databinding.FragmentSearchNewsBinding
+import com.anomalydev.worldnewsforyou.util.onQueryTextSubmit
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
@@ -22,12 +23,14 @@ class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
 
     private val viewModel: SearchNewsViewModel by viewModels()
 
+    private lateinit var newsArticleAdapter: NewsArticlePagingAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentSearchNewsBinding.bind(view)
 
-        val newsArticleAdapter = NewsArticlePagingAdapter(
+        newsArticleAdapter = NewsArticlePagingAdapter(
             onItemClick = { article ->
                 val uri = Uri.parse(article.url)
                 val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -51,9 +54,88 @@ class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
             viewLifecycleOwner.lifecycleScope.launchWhenStarted {
                 viewModel.searchResults.collectLatest { data ->
                     textViewInstructions.isVisible = false
+                    swipeRefreshLayout.isEnabled = true
+
                     newsArticleAdapter.submitData(data)
                 }
             }
+
+            swipeRefreshLayout.isEnabled = false
+
+            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+                newsArticleAdapter.loadStateFlow
+                    .collect { loadState ->
+                        when (val refresh = loadState.mediator?.refresh) {
+                            is LoadState.Loading -> {
+                                textViewError.isVisible = false
+                                buttonRetry.isVisible = false
+                                swipeRefreshLayout.isRefreshing = true
+                                textViewNoResult.isVisible = false
+                                recyclerView.isVisible = newsArticleAdapter.itemCount > 0
+                            }
+                            is LoadState.NotLoading -> {
+                                textViewError.isVisible = false
+                                buttonRetry.isVisible = false
+                                swipeRefreshLayout.isRefreshing = false
+                                recyclerView.isVisible = newsArticleAdapter.itemCount > 0
+
+                                val noResults =
+                                    newsArticleAdapter.itemCount < 1 && loadState.append.endOfPaginationReached
+                                            && loadState.source.append.endOfPaginationReached
+
+                                textViewNoResult.isVisible = noResults
+                            }
+                            is LoadState.Error -> {
+                                swipeRefreshLayout.isRefreshing = false
+                                textViewNoResult.isVisible = false
+                                recyclerView.isVisible = newsArticleAdapter.itemCount > 0
+
+                                val noCacheResults =
+                                    newsArticleAdapter.itemCount < 1 && loadState.source.append.endOfPaginationReached
+
+                                textViewError.isVisible = noCacheResults
+                                buttonRetry.isVisible = noCacheResults
+
+                                val errorMessage = getString(R.string.could_not_load_search_results,
+                                    refresh.error.localizedMessage
+                                        ?: getString(R.string.unknown_error_occurred)
+                                )
+
+                                textViewError.text = errorMessage
+                            }
+                        }
+                    }
+            }
+
+            swipeRefreshLayout.setOnRefreshListener {
+                newsArticleAdapter.refresh()
+            }
+
+            buttonRetry.setOnClickListener {
+                newsArticleAdapter.retry()
+            }
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search_news, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.onQueryTextSubmit { query ->
+            viewModel.onSearchQuerySubmit(query)
+            searchView.clearFocus()
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when(item.itemId) {
+            R.id.action_refresh -> {
+                newsArticleAdapter.refresh()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
