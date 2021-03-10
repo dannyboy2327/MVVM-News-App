dd59e6cb6ddf15bd04e31bc51d3a3aaba993a685
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-10 13:59:14 -0800

    Refactor: changed the class name
    Class changed to SearchResult.

--- app/src/main/java/com/anomalydev/worldnewsforyou/database/NewsArticleDatabase.kt
+++ app/src/main/java/com/anomalydev/worldnewsforyou/database/NewsArticleDatabase.kt
@@ -3,7 +3,7 @@ package com.anomalydev.worldnewsforyou.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [NewsArticle::class, BreakingNews::class, SearchResults::class, SearchQueryRemoteKey::class], version = 1, exportSchema = false)
+@Database(entities = [NewsArticle::class, BreakingNews::class, SearchResult::class, SearchQueryRemoteKey::class], version = 1, exportSchema = false)
 abstract class NewsArticleDatabase: RoomDatabase() {
 
     abstract fun newsArticleDao(): NewsArticleDao
